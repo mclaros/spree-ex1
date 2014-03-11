ce58ca30441ff047436ff97f29860d062bef77d3
Mainor <m_claros@live.com>
2014-03-11 16:16:26 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpreeEx1::Application.config.secret_key_base = 'ca2e36f50b2ebc89291b0fdb5a457ec2484466bc25428cecce03df261d26b243fc2f06353f3eb523579de22eaea04e5821f2af40a9a36bd29f66a65b59f543ca'
