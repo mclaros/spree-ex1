db42fe9283cd5bce0f33f6c2157ccc99c7441327
Mainor <m_claros@live.com>
2014-03-11 19:22:10 -0700

    assume modules are included, add self. to class methods

--- spree_advanced_orders/app/controllers/spree/order_decorator.rb
+++ spree_advanced_orders/app/controllers/spree/order_decorator.rb
@@ -1,13 +1,13 @@
 module Spree
 	Order.class_eval do
 
-		def by_shipment_state(status)
+		def self.by_shipment_state(status)
 			#Alternatively, using ActiveRecord's method_missing magic:
 			#  Spree::Order.find_by_shipment_state(status)
 			@orders = Spree::Order.where(:shipment_state => status)
 		end
 
-		def by_price_range(*range_values)
+		def self.by_price_range(*range_values)
 			min, max = range_values
 
 			if min && max
