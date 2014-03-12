dd4edba5c2db2e72be3a08e68cd621f2aac7460b
Mainor <m_claros@live.com>
2014-03-11 19:18:26 -0700

    order decorator

---
+++ spree_advanced_orders/app/controllers/spree/order_decorator.rb
@@ -0,0 +1,24 @@
+module Spree
+	Order.class_eval do
+
+		def by_shipment_state(status)
+			#Alternatively, using ActiveRecord's method_missing magic:
+			#  Spree::Order.find_by_shipment_state(status)
+			@orders = Spree::Order.where(:shipment_state => status)
+		end
+
+		def by_price_range(*range_values)
+			min, max = range_values
+
+			if min && max
+				@orders = Spree::Order.where('total BETWEEN ? AND ?', min, max)
+			elsif min
+				@orders = Spree::Order.where('total >= ?', min)
+			elsif max
+				@orders = Spree::Order.where('total <= ?', max)
+			end
+		end
+
+	end
+
+end
